8060865695adadc0cd464b4dc8abef6f7315f007
ela.md <ela.ahmadi06@gmail.com>
2025-02-06 15:19:59 -0500

    add context file

---
+++ src/context.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AppContext = createContext();
+
+function AppProvider ({children}) {
+
+     return (
+        <AppContext.Provider value={{}}>
+           {children}
+        </AppContext.Provider>
+     )
+}
+
+export const useGlobalContext = () => useContext(AppContext)
+
+
+
+export default AppProvider
