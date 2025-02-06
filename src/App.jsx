c47a4b51eb25106cd678a34d7a510452afbfaff4
ela.md <ela.ahmadi06@gmail.com>
2025-02-06 14:44:49 -0500

    folder structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import ThemeToggle from "./ThemeToggle";
+
+function App ()  {
+  return (
+
+   <>
+   <Gallery />
+   <SearchForm />
+   <ThemeToggle />   
+   </>
+  )
+};
+export default App;
