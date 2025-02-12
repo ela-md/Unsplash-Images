bf45438d9d3e874e39f41ea7347a18dffd3a5ec6
ela.md <ela.ahmadi06@gmail.com>
2025-02-12 13:54:52 -0500

    setup SearchForm

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,6 +1,23 @@
 function SearchForm () {
+
+ const handleSubmit = (e) =>{
+    e.prevenyDefault();
+    const searchValue = e.target.elements.search.value
+    if(!searchValue) return
+ };
+
     return(
-        <h2>SearchForm</h2>
+        <section>
+           <h1 className="titile">Unsplash Images</h1> 
+           <form className="search-form" onSubmit={handleSubmit}>
+            <input type="text" className="form-input search-input" 
+            name="search" placeholder="cat" />
+            <button type="submit" className="btn">
+                search
+            </button>
+
+           </form>
+        </section>
     );
 }
 
