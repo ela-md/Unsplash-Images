1e5b0058bb85cb2bdc995ca0d6facfe474141a9c
ela.md <ela.ahmadi06@gmail.com>
2025-02-13 14:58:57 -0500

    setup query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url = 'https://api.unsplash.com/search/photos?client_id=KQP31iEegDQWe3CuZzYJfepLyJSzJ4lw9Nz4QPDlgyI&query=office';
+
 function Gallery () {
+  
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.getUri(url);
+      return result.data
+    },
+  });
+  console.log(response);
+  
     return(
         <h2>Gallery</h2>
     );
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -4,11 +4,12 @@ function SearchForm () {
     e.prevenyDefault();
     const searchValue = e.target.elements.search.value
     if(!searchValue) return
+    
  };
 
     return(
         <section>
-           <h1 className="titile">Unsplash Images</h1> 
+           <h1 className="title">Unsplash Images</h1> 
            <form className="search-form" onSubmit={handleSubmit}>
             <input type="text" className="form-input search-input" 
             name="search" placeholder="cat" />
